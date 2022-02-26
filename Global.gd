61e5e6e40defc854252db750e5b466d38bee086d
losten25 <97627275+losten25@users.noreply.github.com>
2022-02-26 11:40:33 -0500

    mid exercise question

--- Global.gd
+++ Global.gd
@@ -1,7 +1,15 @@
 extends Node
 
 var score = 0
-signal changed
+var time = 0
+
+var level = 1
+
+var level1_default = 60
+
+signal score_changed
+signal time_changed
+
 var scores = {
 	0:0,
 	1:0,
@@ -15,6 +23,10 @@ var scores = {
 	9:1000
 }
 
+func _ready():
+	if level == 1:
+		time = level1_default
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
@@ -24,4 +36,13 @@ func change_score(s):
 	if camera:
 		camera.add_trauma(0.5)
 	score += s
-	emit_signal("changed")
+	emit_signal("score_changed")
+
+func change_time():
+	if level == 1:
+		time -= 1
+	else:
+		time += 1
+	time += 1
+	emit_signal("time_changed")
+
